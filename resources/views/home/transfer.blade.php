84c2e2468de5182bc73050ea3d6a7403a5a4b912
sezer57 <sezer815@gmail.com>
2022-05-03 02:58:58 +0300

    detail page

---
+++ resources/views/home/transfer.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.frontbase')
+
+@section('title','HavaAlanıTransfer')
+{{--@section('sidebar')--}}
+{{--    @parent--}}
+
+{{--@endsection--}}
+@section('content') <main id="main">
+
+
+
+
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="icon-box">
+                        <i class="bi bi-briefcase"></i>
+                        <h4><a href="#">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-card-checklist"></i>
+                        <h4><a href="#">Dolor Sitema</a></h4>
+                        <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-bar-chart"></i>
+                        <h4><a href="#">Sed ut perspiciatis</a></h4>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-binoculars"></i>
+                        <h4><a href="#">Nemo Enim</a></h4>
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-brightness-high"></i>
+                        <h4><a href="#">Magni Dolore</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-calendar4-week"></i>
+                        <h4><a href="#">Eiusmod Tempor</a></h4>
+                        <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+
+
+
+            <div class="row portfolio-container">
+
+                @foreach($transferlist as $rs)
+
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="portfolio-wrap"> <a href="portfolio-details.html" >
+                        <img src="{{Storage::url($rs->image)}}" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+
+                            <h4>{{$rs->transfer->title}}</h4>
+
+                            <div class="portfolio-links">
+                                <a href="{{Storage::url($rs->image)}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div></a>
+                    </div>
+                </div>
+                @endforeach
+
+            </div>
+
+        </div>
+    </section><!-- End Portfolio Section -->
+
+</main><!-- End #main -->
+
+@endsection
